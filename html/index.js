00ab8babc5c60c306958d20e7c208517feb12f53
Diego <Diegofla10@outlook.com>
2025-04-06 13:27:51 -0300

    1.2
    - Cores levemente alteradas.
    - Correção no arquivo index.js que antes não validava corretamente a entrada do usuario.
    - Exclusão de linhas desnecessarias.

--- html/index.js
+++ html/index.js
@@ -1,42 +1,41 @@
 function verificarSenha(event) {
-    event.preventDefault();
+    event.preventDefault(); 
 
+    let primeiraSenha = document.querySelector('.firstPassword').value.trim();
+    let segundaSenha = document.querySelector('.secondPassword').value.trim();
+    let nome = document.getElementById('campoNome').value.trim();
 
-    let primeiraSenha = document.querySelector('.firstPassword').value;
-    let segundaSenha = document.querySelector('.secondPassword').value;
-   
     let temLetraMaiuscula = /[A-Z]/.test(primeiraSenha);
     let temLetraMinuscula = /[a-z]/.test(primeiraSenha);
-    let temNumero = /\d/.test(primeiraSenha);
 
+    let temNumero = /\d/.test(primeiraSenha);
 
     if (primeiraSenha.length < 8) {
         alert("A senha deve ter pelo menos 8 caracteres.");
         return false;
     }
+
     if (!temLetraMaiuscula || !temLetraMinuscula || !temNumero) {
         alert("A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número.");
         return false;
     }
+
     if (primeiraSenha !== segundaSenha) {
         alert("As senhas não coincidem.");
         return false;
     }
 
+    if (!/^[a-zA-ZÀ-ÿ\s]+$/.test(nome)) {
+        alert("Nome inválido! Use apenas letras.");
+        return false;
+    }
+    
 
     alert("Cadastro realizado com sucesso!");
+
+    setTimeout(function() {
+        window.location.href = "sucessoCadastro.html";
+    }, 1000);
+
     return true;
 }
-
-if(verificarSenha == true){
-    changePage();
-}
-
-
-function changePage(){
-    setTimeout(function(){
-        window.location.href = "sucessoCadastro.html";
-    }, 3000);
-}
-
-
