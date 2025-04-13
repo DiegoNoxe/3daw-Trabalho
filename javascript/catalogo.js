269c32ad9807eda4dd5d7da70441294529f0ba77
Eduardo <edusb1613@gmail.com>
2025-04-12 22:35:08 -0300

    script para filtrar catalogo

--- javascript/catalogo.js
+++ javascript/catalogo.js
@@ -1,8 +1,30 @@
 function alternarInteresse(botao) {
-    botao.classList.toggle("interesse-ativo");
-    if (botao.classList.contains("interesse-ativo")) {
-      botao.textContent = "Tirar Interesse";
-    } else {
-      botao.textContent = "Tenho interesse";
-    }
+  botao.classList.toggle("interesse-ativo");
+  if (botao.classList.contains("interesse-ativo")) {
+    botao.textContent = "Tirar Interesse";
+  } else {
+    botao.textContent = "Tenho interesse";
   }
+  filtrar();
+}
+
+
+function filtrar() {
+  const filtroSelecionado = document.querySelector('input[name="filtro"]:checked').id;
+  const produtos = document.querySelectorAll('.produto');
+
+  produtos.forEach(produto => {
+    const botao = produto.querySelector('.botao-interesse');
+    const temInteresse = botao.classList.contains('interesse-ativo');
+
+    if (filtroSelecionado === 'todos') {
+      produto.style.display = 'flex';
+    } else if (filtroSelecionado === 'interesse') {
+      produto.style.display = temInteresse ? 'flex' : 'none';
+    }
+    });
+        }
+
+document.querySelectorAll('input[name="filtro"]').forEach(radio=>{
+  radio.addEventListener('change', filtrar);
+})
